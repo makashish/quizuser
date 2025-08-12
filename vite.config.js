4b8f8d07fccf7f4d9a879755cb2ffcff4d1a4bd8
makanindian <makanindian@gmail.com>
2025-08-12 23:36:01 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()],
-  base: './', // ensures relative paths for assets
+  build: {
+    outDir: 'dist',   // default 'dist', agar yeh change hua ho to us folder ko set karo
+  },
+  base: './',        // ya koi relative base
 });
