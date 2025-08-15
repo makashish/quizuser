f158c171cfaa9859e9b13a22711f1dac48b38c6b
makashish <makanindian@gmail.com>
2025-08-15 14:43:54 +0530

    Updated Features

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  plugins: [react()],
+
+  // Path your app will be served from
+  base: './', // or '/my-subfolder/' if hosted in a subdirectory
+
   build: {
-    outDir: 'dist',   // default 'dist', agar yeh change hua ho to us folder ko set karo
-  },
-  base: './',        // ya koi relative base
-});
+    outDir: 'dist', // default output folder (you can change to 'build' or something else)
+  }
+})
